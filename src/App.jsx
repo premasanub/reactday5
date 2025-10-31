c698c336b4553c4e86ac2d4c33d3ffdad7a16caf
premasanub <premakumaran15@gmail.com>
2025-10-31 20:29:24 +0530

    basic setup completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Forms from './Compoinents/Forms';
+
+const App = () => {
+  return (
+    <div>
+      
+      <Forms />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Compoinents/Forms.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Forms = () => {
+    return (
+        <div>
+            <h1>forms</h1>
+        </div>
+    );
+};
+
+export default Forms;
